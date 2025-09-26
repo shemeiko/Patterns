79c1306d7175880423e2957de555bf639c556dc8
Taras Shemeiko <shemiks2005@gmail.com>
2025-09-26 18:38:03 +0300

    init

---
+++ Singleton.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+class Logger {
+private:
+    Logger() {}
+    ~Logger() {}
+public:
+    Logger(const Logger&) = delete;
+    void operator=(const Logger&) = delete;
+    Logger(const Logger&&) = delete;
+    void operator=(const Logger&&) = delete;
+
+    static Logger& getLogger() {
+        static Logger instance;
+        return instance;
+    }
+
+    void log(std::string log_message) {
+        std::cout << log_message << '\n';
+    }
+};
+
+int main() {
+    auto& logger = Logger::getLogger();
+    std::cout << &logger << " " << &Logger::getLogger() << '\n';
+    logger.log("Program successfully completed");
+}
